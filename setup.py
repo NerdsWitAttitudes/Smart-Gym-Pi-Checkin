b43ab12d9a2bbbaa322d49ec5e20508a1b9f2382
Robin Siep <RobinSiep1@gmail.com>
2016-05-19 10:45:09 +0200

    setup py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.md')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pybluez',
+    'requests'
+]
+
+setup(name='smartgympi',
+      version='0.1',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python"
+      ],
+      author='Nerds Wit Attitudes',
+      author_email='',
+      keywords='pybluez',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='smartgympi',
+      install_requires=requires
+      )
